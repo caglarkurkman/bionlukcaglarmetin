8d60e98e121d1c44efe50ff1fba626686766c2ef
metintas <metintas035@gmail.com>
2025-01-04 09:54:12 +0300

    anasayfa duzenlemesi yapildi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Bloglarimiz from "./pages/Bloglarimiz";
 import Categories from "./pages/Categories";
+import Homepage from './pages/Homepage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Categories />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/bloglarimiz" element={<Bloglarimiz />} />
       </Routes>
     </Router>
